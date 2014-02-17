fb410929093890e6764cda968968b7393c171f26
Courtland Caldwell <courtland.caldwell@cympel.com>
2014-02-17 16:29:06 -0600

    Initial commit of learningRuby

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '46e374181e74ea0278d43bb3eda322ced2ef23aaa7f3219d344b0c80e30ca816cd3f6db5b91a420f388aa8b54313b975c7e974f6035414b33624fb7caa6d4162'
---
+++ practice/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Practice::Application.config.secret_key_base = '870ba9f5ac0255878edb4454d538c096b18917f8480c44bb4f8a00af56824445763e0573f9c36a7c4b4b7d2cc01c95b566db8c1f5b0855f117f43672daf7d108'
